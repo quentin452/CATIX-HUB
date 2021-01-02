56cdceedd3ddb75879a49c9093365e7d65e7894b
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-01-02 20:17:10 +0100

    Update Coin Hero Simulator GUI.lua

--- Coin Hero Simulator GUI.lua
+++ Coin Hero Simulator GUI.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://zypher.wtf/UI-Lib"))()
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/H5u3dYwA"))()
 
 	local main = library:CreateMain({
 		projName = "ScreenGui",
