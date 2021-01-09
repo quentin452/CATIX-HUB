f8f166b75dad6dc7677a58f5a46f52ded4be4324
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-01-09 11:14:58 +0100

    Update Saber Simulator GUI.lua

--- Saber Simulator GUI.lua
+++ Saber Simulator GUI.lua
@@ -23,7 +23,7 @@ local Virtual = game:service'VirtualUser'
     wait(2)
 end)
 
-local library = loadstring(game:HttpGet("https://pastebin.com/raw/H5u3dYwA"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/quentin452/CATIX-HUB/master/!Zypher%20UI.lua"))()
 local main = library:CreateMain("zypher")
 
 local Home = main:CreateCategory("Home")
