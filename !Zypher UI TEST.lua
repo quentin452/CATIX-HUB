88f3d89c84368e7101aba037cb3aced5569aac8b
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-01-10 21:09:58 +0100

    Update !Zypher UI TEST.lua

--- !Zypher UI TEST.lua
+++ !Zypher UI TEST.lua
@@ -99,12 +99,18 @@ function Library:CreateMain(Options)
 		end
 	end)
 
-	Main.Upline = Library:Create("Frame", {
-		Name = "Upline",
-		BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-		BorderSizePixel = 0,
-		Size = UDim2.new(1, 0, 0.0189999994, 0),
-		ZIndex = -12,
+	Main.Upline = Library:Create("ImageLabel", {
+     Name = "Top",
+        BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+		BackgroundTransparency = 1.000,
+		Position = UDim2.new(0, 0, -1.25701344, 0),
+		Size = UDim2.new(1, 0, 2.66662264, 10),
+		ZIndex = -10,
+		Image = "rbxassetid://2851926732",
+		ImageColor3 = Color3.fromRGB(14790, 0, 0),
+		ScaleType = Enum.ScaleType.Slice,
+		SliceCenter = Rect.new(12, 12, 12, 12),
+	
 	})
 
 	Main.Uplinegradient = Library:Create("UIGradient", {
@@ -114,20 +120,8 @@ function Library:CreateMain(Options)
 			ColorSequenceKeypoint.new(1.00, Color3.fromRGB(125, 92, 164))
 		}
 	})
-    Main.UplineTop = Library:Create("Top", {
-        BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		BackgroundTransparency = 1.000
-		Position = UDim2.new(0, 0, -1.25701344, 0)
-		Size = UDim2.new(1, 0, 2.66662264, 10)
-		ZIndex = -10
-		Image = "rbxassetid://2851926732"
-		ImageColor3 = Color3.fromRGB(14790, 0, 0)
-		ScaleType = Enum.ScaleType.Slice
-		SliceCenter = Rect.new(12, 12, 12, 12)
-		 
-	})
 	Main.Sidebar = Library:Create("ScrollingFrame", {
-		Name = "Sidebar",
+	
 		Active = true,
 		BackgroundColor3 = Themes.GrayContrast,
 		BorderSizePixel = 0,
