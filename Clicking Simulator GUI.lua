318d0a4dc5508f83d034ba71bd308e1129e58d0e
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-02-24 21:00:22 +0100

    Update Clicking Simulator GUI.lua

--- Clicking Simulator GUI.lua
+++ Clicking Simulator GUI.lua
@@ -1,33 +1,26 @@
 --https://www.roblox.com/games/3462993756/Clicking-Simulator
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
 
-
 local AutoTab = Library:CreateTab("Clicking Simulator GUI","things", true)
 local teleports = Library:CreateTab("Teleports","things", true)
 local MISC = Library:CreateTab("MISC","things", true)
 local credits = Library:CreateTab("Credits","things", true)
-
 credits:CreateButton("Our Discords", function() --you dont need "arg" for a button
     setclipboard("AHAHAHAHAH")
 end)
-
 credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() --you dont need "arg" for a button
     setclipboard("https://discord.gg/KmHZUpXEmQ")
 end)
-
 credits:CreateButton("by Altix#3395", function() --you dont need "arg" for a button
     setclipboard("Altix#3395")
 end)
-
 credits:CreateButton("by I'm A Cat#7202", function() --you dont need "arg" for a button
     setclipboard("I'm A Cat#7202")
 end)
 
-
-
-AutoTab:CreateToggle("Auto Click", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Click", function(bool) --the (arg) is if the checkbox is toggled or not
     if arg then
-        _G.a = true 
+        _G.a = bool 
         while wait(0) and _G.a do
            local tool = game.Players.LocalPlayer.Backpack:FindFirstChildOfClass("Tool") or game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
     game.Players.LocalPlayer.Character.Humanoid:EquipTool(tool)
@@ -35,9 +28,6 @@ AutoTab:CreateToggle("Auto Click", function(arg) --the (arg) is if the checkbox 
         game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):Activate()
     end
 end
-
-    else
-        _G.a = false
     end
 end)
 
@@ -51,7 +41,6 @@ end)
 plr = game.Players.LocalPlayer
 mouse = plr:GetMouse()
 mouse.KeyDown:connect(function(key)
-
 if key == "e" then
 noclip = not noclip
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
@@ -60,7 +49,6 @@ end)
 print('Created by FileExplorer')
 print('Credits to Peyton for the ChangeState part of this script')
 end)
-
 MISC:CreateButton("Inf Jump", function(arg) --the (arg) is if the checkbox is toggled or not
   InfiniteJumpEnabled = true
 game:GetService("UserInputService").JumpRequest:connect(function()
@@ -69,35 +57,27 @@ game:GetService("UserInputService").JumpRequest:connect(function()
 	end
 end)
 end)
-
 MISC:CreateButton("Speed Hack 100", function(arg) --the (arg) is if the checkbox is toggled or not
             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
 end)
-
 AutoTab:CreateButton("Destroy Gui :D", function() --you dont need "arg" for a button
 game:GetService("CoreGui").uiui:Destroy()
 end)
-
 teleports:CreateButton("Last Island", function() --you dont need "arg" for a button
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-12620.4941, 9.63047886, -14877.0605)
 end)
-
 teleports:CreateButton("Spawn", function() --you dont need "arg" for a button
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(401.053375, 185.463593, -2639.55029)
 end)
-
 teleports:CreateButton("Shop", function() --you dont need "arg" for a button
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(347.711365, 184.789627, -2643.66333)
 end)
-
 teleports:CreateButton("LeaderBoards", function() --you dont need "arg" for a button
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(488.097565, 184.869598, -2540.67334)
 end)
-
 teleports:CreateButton("Rebirths", function() --you dont need "arg" for a button
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(303.511963, 185.362595, -2498.69287)
 end)
-
 --| Anti AFK | By Altix#3395
     local Virtual = game:service'VirtualUser'
     game:service'Players'.LocalPlayer.Idled:connect(function()
