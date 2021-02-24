51221cd0531c7e9680f96ba79f078b9b4c6b26f6
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-02-24 20:32:41 +0100

    Update Coin Collection Simulator GUI.lua

--- Coin Collection Simulator GUI.lua
+++ Coin Collection Simulator GUI.lua
@@ -1,125 +1,84 @@
 --https://www.roblox.com/games/5093293793/HUGE-UPDATE-Coin-Collection-Simulator
-
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
-
 local AutoTab = Library:CreateTab("Coin Collection Simulator GUI","things", true)
 local teleports = Library:CreateTab("Teleports","things", true)
 local eggs = Library:CreateTab("Auto By Cages SLOW","things", true)
 local MISC = Library:CreateTab("MISC","things", true)
 local credits = Library:CreateTab("Credits","things", true)
-
-
-credits:CreateButton("Our Discords", function() --you dont need "arg" for a button
+credits:CreateButton("Our Discords", function() 
     setclipboard("AHAHAHAHAH")
 end)
-
-credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() --you dont need "arg" for a button
+credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() 
     setclipboard("https://discord.gg/KmHZUpXEmQ")
 end)
-
-credits:CreateButton("by Altix#3395", function() --you dont need "arg" for a button
+credits:CreateButton("by Altix#3395", function() 
     setclipboard("Altix#3395")
 end)
-
-credits:CreateButton("by I'm A Cat#7202", function() --you dont need "arg" for a button
+credits:CreateButton("by I'm A Cat#7202", function() 
     setclipboard("I'm A Cat#7202")
 end)
-
-
-eggs:CreateToggle("Auto Buy Cages 1", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.a = true 
+eggs:CreateToggle("Auto Buy Cages 1", function(bool) 
+        _G.a = bool 
         while wait(0) and _G.a do
            local args = {
     [1] = "StarterCage",
 }
 
 workspace.petInfo:InvokeServer(unpack(args))
-        end
-    else
-        _G.a = false
     end
 end)
-
-eggs:CreateToggle("Auto Buy Cages 2", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.b = true 
+eggs:CreateToggle("Auto Buy Cages 2", function(bool) 
+        _G.b = bool 
         while wait(0) and _G.b do
-         local args = {
-    [1] = "DesertCage",
-}
-
-workspace.petInfo:InvokeServer(unpack(args))
+workspace.petInfo:InvokeServer("DesertCage")
         end
-    else
-        _G.b = false
-    end
 end)
-
-eggs:CreateToggle("Auto Buy Cages 3", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.c = true 
+eggs:CreateToggle("Auto Buy Cages 3", function(bool) 
+        _G.c = bool 
         while wait(0) and _G.c do
          local args = {
     [1] = "SnowCage",
 }
 
 workspace.petInfo:InvokeServer(unpack(args))
-        end
-    else
-        _G.c = false
     end
 end)
 
-eggs:CreateToggle("Auto Buy Cages 4", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.d = true 
+eggs:CreateToggle("Auto Buy Cages 4", function(bool) 
+        _G.d = bool 
         while wait(0) and _G.d do
          local args = {
     [1] = "CityCage",
 }
 
 workspace.petInfo:InvokeServer(unpack(args))
-        end
-    else
-        _G.d = false
     end
 end)
 
-eggs:CreateToggle("Auto Buy Cages 5", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.e = true 
+eggs:CreateToggle("Auto Buy Cages 5", function(bool) 
+        _G.e = bool 
         while wait(0) and _G.e do
          local args = {
     [1] = "LavaCage",
 }
 
 workspace.petInfo:InvokeServer(unpack(args))
-        end
-    else
-        _G.e = false
     end
 end)
 
-eggs:CreateToggle("Auto Buy Cages 6", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.f = true 
+eggs:CreateToggle("Auto Buy Cages 6", function(bool) 
+        _G.f = bool 
         while wait(0) and _G.f do
          local args = {
     [1] = "OceanCage",
 }
 
 workspace.petInfo:InvokeServer(unpack(args))
-        end
-    else
-        _G.f = false
     end
 end)
 
-eggs:CreateToggle("Auto Buy Cages 7", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.g = true 
+eggs:CreateToggle("Auto Buy Cages 7", function(bool) 
+        _G.g = bool 
         while wait(0) and _G.g do
          local args = {
     [1] = "CandyCage",
@@ -127,29 +86,21 @@ eggs:CreateToggle("Auto Buy Cages 7", function(arg) --the (arg) is if the checkb
 
 workspace.petInfo:InvokeServer(unpack(args))
         end
-    else
-        _G.g = false
-    end
 end)
 
-eggs:CreateToggle("Auto Buy Cages 8", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.h = true 
+eggs:CreateToggle("Auto Buy Cages 8", function(bool) 
+        _G.h = bool 
         while wait(0) and _G.h do
          local args = {
     [1] = "JungleCage",
 }
 
 workspace.petInfo:InvokeServer(unpack(args))
-        end
-    else
-        _G.h = false
     end
 end)
 
-eggs:CreateToggle("Auto Buy Cages 9", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.i = true 
+eggs:CreateToggle("Auto Buy Cages 9", function(bool) 
+        _G.i = bool 
         while wait(0) and _G.i do
       local args = {
     [1] = "SpaceCage",
@@ -157,28 +108,21 @@ eggs:CreateToggle("Auto Buy Cages 9", function(arg) --the (arg) is if the checkb
 
 workspace.petInfo:InvokeServer(unpack(args))
         end
-    else
-        _G.i = false
-    end
 end)
 
-AutoTab:CreateToggle("Auto Click", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.j = true 
+AutoTab:CreateToggle("Auto Click", function(bool) 
+        _G.j = bool 
         while wait(0) and _G.j do
-            if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
-            game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):Activate()
+             local tool = game.Players.LocalPlayer.Backpack:FindFirstChildOfClass("Tool") or game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
+    game.Players.LocalPlayer.Character.Humanoid:EquipTool(tool)
+    if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
+        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):Activate()
     end
-end
-
-    else
-        _G.j = false
     end
 end)
 
-MISC:CreateToggle("NO CLIP Press(E)", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-_G.NO = true 
+MISC:CreateButton("🎬NO CLIP Press(E)🎬", function() 
+    _G.NO2 = true 
   noclip = false
 game:GetService('RunService').Stepped:connect(function()
 if noclip then
@@ -196,132 +140,216 @@ end
 end)
 print('Created by FileExplorer')
 print('Credits to Peyton for the ChangeState part of this script')
+end)
 
-         
-else
-_G.NO = false
+MISC:CreateButton("🤸‍♀️Inf Jump🤸‍♀️", function() 
+game:GetService("UserInputService").JumpRequest:connect(function()
+    game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+end)
+end)
+MISC:CreateButton("🕹️Ctrl + Click = TP🕹️", function() 
+local Plr = game:GetService("Players").LocalPlayer
+local Mouse = Plr:GetMouse()
+Mouse.Button1Down:connect(function()
+if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then return end
+if not Mouse.Target then return end
+Plr.Character:MoveTo(Mouse.Hit.p)
+end)
+end)
+
+MISC:CreateButton("🪰Fly Press (F)🪰", function() 
+     repeat wait() 
+	until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
+local mouse = game.Players.LocalPlayer:GetMouse() 
+repeat wait() until mouse
+local plr = game.Players.LocalPlayer 
+local torso = plr.Character.Head 
+local flying = false
+local deb = true 
+local ctrl = {f = 0, b = 0, l = 0, r = 0} 
+local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+local maxspeed = 400 
+local speed = 5000 
+function Fly() 
+local bg = Instance.new("BodyGyro", torso) 
+bg.P = 9e4 
+bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
+bg.cframe = torso.CFrame 
+local bv = Instance.new("BodyVelocity", torso) 
+bv.velocity = Vector3.new(0,0.1,0) 
+bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
+repeat wait() 
+plr.Character.Humanoid.PlatformStand = true 
+if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
+speed = speed+.5+(speed/maxspeed) 
+if speed > maxspeed then 
+speed = maxspeed 
+end 
+elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
+speed = speed-1 
+if speed < 0 then 
+speed = 0 
+end 
+end 
+if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
+elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+else 
+bv.velocity = Vector3.new(0,0.1,0) 
+end 
+bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
+until not flying 
+ctrl = {f = 0, b = 0, l = 0, r = 0} 
+lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+speed = 100
+bg:Destroy() 
+bv:Destroy() 
+plr.Character.Humanoid.PlatformStand = false 
+end 
+mouse.KeyDown:connect(function(key) 
+if key:lower() == "f" then 
+if flying then flying = false 
+else 
+flying = true 
+Fly() 
+end 
+elseif key:lower() == "w" then 
+ctrl.f = 1 
+elseif key:lower() == "s" then 
+ctrl.b = -1 
+elseif key:lower() == "a" then 
+ctrl.l = -1 
+elseif key:lower() == "d" then 
+ctrl.r = 1 
+end 
+end) 
+mouse.KeyUp:connect(function(key) 
+if key:lower() == "w" then 
+ctrl.f = 0 
+elseif key:lower() == "s" then 
+ctrl.b = 0 
+elseif key:lower() == "a" then 
+ctrl.l = 0 
+elseif key:lower() == "d" then 
+ctrl.r = 0 
+end 
+end)
+Fly()
+
+          
+end)
+
+MISC:CreateButton("🛡️Infinite Yield🛡️", function() 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+MISC:CreateButton("💨Speed Hack 200 PRESS (R)💨", function() 
+   
+local walkspeedplayer = game:GetService("Players").LocalPlayer
+local walkspeedmouse = walkspeedplayer:GetMouse()
+
+local walkspeedenabled = false
+
+function x_walkspeed(key)
+if (key == "r") then
+if walkspeedenabled == false then
+_G.WS = 200;
+local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+Humanoid.WalkSpeed = _G.WS;
+end)
+Humanoid.WalkSpeed = _G.WS;
+
+walkspeedenabled = true
+elseif walkspeedenabled == true then
+_G.WS = 20;
+local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+Humanoid.WalkSpeed = _G.WS;
+end)
+Humanoid.WalkSpeed = _G.WS;
+
+walkspeedenabled = false
 end
+end
+end
+
+walkspeedmouse.KeyDown:connect(x_walkspeed)
+
 end)
 
 
 
-MISC:CreateToggle("Speed Hack 100", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-_G.Speed = true 
-        while wait() and _G.Speed do
-            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
-        end
-    else
-        _G.Speed = false
-           game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 20
-         end
-end)
-AutoTab:CreateToggle("Disable all pop ups", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.l = true 
+
+AutoTab:CreateToggle("Disable all pop ups", function(bool) 
+        _G.l = bool 
         while wait(0) and _G.l do
           game:GetService("ReplicatedStorage").CoinFrame:Destroy()
           game:GetService("ReplicatedStorage").XpFrame:Destroy()  
         end
-    else
-        _G.l = false
-    end
 end)
 
-AutoTab:CreateButton("Tp to Last Zone", function() --you dont need "arg" for a button
-game:GetService("CoreGui").uiui:nothing()
-end)
-
-AutoTab:CreateButton("And Execute Auto Coins", function() --you dont need "arg" for a button
-game:GetService("CoreGui").uiui:nothing()
-end)
-
-AutoTab:CreateToggle("Auto Coins FAST", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.m = true 
-        while wait(0) and _G.m do
-             for i,v in pairs (game:GetService("Workspace").AllCoins.SpaceCoin:GetChildren()) do
-        v.Coin.CFrame = Game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-        end
-end
-    else
-        _G.m = false
-    end
-end)
-
-AutoTab:CreateToggle("Auto Sacrifices", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.n = true 
-        while wait(0) and _G.n do
-          local args = {
-    [1] = 0,
-}
-
-game:GetService("ReplicatedStorage").SacrificeMoney:FireServer(unpack(args))
-end
-    else
-        _G.n = false
-    end
-end)
-
-teleports:CreateButton("Shop", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-319.3284, 6.41753483, -571.995544)
-end)
-
-teleports:CreateButton("Zone 1", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-11.0445356, 6.05753565, -546.400757)
-end)
-
-teleports:CreateButton("Zone 2", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(316.360413, 6.05753565, -570.571411)
-end)
-
-teleports:CreateButton("Zone 3", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(694.087219, 6.05384827, -561.044739)
-end)
-
-teleports:CreateButton("Zone 4", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(1087.39343, 5.5575366, -553.410522)
-end)
-
-teleports:CreateButton("Zone 5", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(1423.27222, 6.0542655, -549.605042)
-end)
-
-teleports:CreateButton("Zone 6", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(1779.07678, 6.05753422, -555.153259)
-end)
-
-teleports:CreateButton("Zone 7", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(2144.20239, 6.05753565, -501.873474)
-end)
-
-teleports:CreateButton("Zone 8", function() --you dont need "arg" for a button
-game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(2513.57397, 6.05426502, -511.89859)
-end)
-
-teleports:CreateButton("Zone 9", function() --you dont need "arg" for a button
+AutoTab:CreateButton("Tp to Last Zone", function() 
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(2883.66211, 6.05753565, -504.741577)
 end)
 
-AutoTab:CreateButton("Destroy Gui :D", function() --you dont need "arg" for a button
-game:GetService("CoreGui").uiui:Destroy()
+AutoTab:CreateButton("And Execute Auto Coins", function() 
 end)
 
-    local vu = game:GetService("VirtualUser")
+AutoTab:CreateToggle("Auto Coins FAST", function(bool) 
+        _G.m = bool 
+        while wait(0) and _G.m do
+             for i,v in pairs (game:GetService("Workspace").AllCoins.SpaceCoin:GetChildren()) do
+        v.Coin.CFrame = Game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+                wait(1)
+    v.CFrame = CFrame.new(0,0,0)
+end
+    end
+end)
+
+AutoTab:CreateToggle("Auto Sacrifices", function(bool) 
+        _G.n = bool 
+        while wait(0) and _G.n do
+game:GetService("ReplicatedStorage").SacrificeMoney:FireServer(0)
+    end
+end)
+teleports:CreateButton("Shop", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-319.3284, 6.41753483, -571.995544)
+end)
+teleports:CreateButton("Zone 1", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-11.0445356, 6.05753565, -546.400757)
+end)
+teleports:CreateButton("Zone 2", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(316.360413, 6.05753565, -570.571411)
+end)
+teleports:CreateButton("Zone 3", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(694.087219, 6.05384827, -561.044739)
+end)
+teleports:CreateButton("Zone 4", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(1087.39343, 5.5575366, -553.410522)
+end)
+teleports:CreateButton("Zone 5", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(1423.27222, 6.0542655, -549.605042)
+end)
+teleports:CreateButton("Zone 6", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(1779.07678, 6.05753422, -555.153259)
+end)
+teleports:CreateButton("Zone 7", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(2144.20239, 6.05753565, -501.873474)
+end)
+teleports:CreateButton("Zone 8", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(2513.57397, 6.05426502, -511.89859)
+end)
+teleports:CreateButton("Zone 9", function() 
+game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(2883.66211, 6.05753565, -504.741577)
+end)
+AutoTab:CreateButton("Destroy Gui :D", function() 
+game:GetService("CoreGui").uiui:Destroy()
+end)
+local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
-
-local ot = mt.__namecall
-mt.__namecall = function(self, ...)
-   local args = {...}
-   local method = namecallMethod()
-   if tostring(method) == "Kick" then
-      print('Kick attempt')
-       return
-   end
-   return ot(self, ...)
-end
