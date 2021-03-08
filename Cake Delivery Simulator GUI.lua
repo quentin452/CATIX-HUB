ecbc05a6096531851eb502bbf1ade761a3cc0fc9
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-03-09 00:17:09 +0100

    Update Cake Delivery Simulator GUI.lua

--- Cake Delivery Simulator GUI.lua
+++ Cake Delivery Simulator GUI.lua
@@ -1,32 +1,27 @@
---https://www.roblox.com/games/3011348948/NEW-Cake-Delivery-Simulator
-
-
+--https://www.roblox.com/games/3011348948/
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
 
 local AutoTab = Library:CreateTab("Cake-Delivery-Simulator GUI","things", true)
 local MISC = Library:CreateTab("MISC","things", true)
 local credits = Library:CreateTab("Credits","things", true)
 
-
-credits:CreateButton("Our Discords", function() --you dont need "arg" for a button
+credits:CreateButton("Our Discords", function() 
     setclipboard("AHAHAHAHAH")
 end)
 
-credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() --you dont need "arg" for a button
+credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() 
     setclipboard("https://discord.gg/KmHZUpXEmQ")
 end)
 
-credits:CreateButton("by Altix#3395", function() --you dont need "arg" for a button
+credits:CreateButton("by Altix#3395", function() 
     setclipboard("Altix#3395")
 end)
 
-credits:CreateButton("by I'm A Cat#7202", function() --you dont need "arg" for a button
+credits:CreateButton("by I'm A Cat#7202", function() 
     setclipboard("I'm A Cat#7202")
 end)
 
-
-
-MISC:CreateToggle("NO CLIP Press(E)", function(arg) --the (arg) is if the checkbox is toggled or not
+MISC:CreateToggle("NO CLIP Press(E)", function(arg) 
     if arg then
 _G.NO = true 
   noclip = false
@@ -38,39 +33,28 @@ end)
 plr = game.Players.LocalPlayer
 mouse = plr:GetMouse()
 mouse.KeyDown:connect(function(key)
-
 if key == "e" then
 noclip = not noclip
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
 end
-end)
-print('Created by FileExplorer')
-print('Credits to Peyton for the ChangeState part of this script')
-
-         
+end) 
 else
 _G.NO = false
 end
 end)
 
-
-
-
-AutoTab:CreateToggle("Auto Click", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.a = true 
+AutoTab:CreateToggle("Auto Click", function(bool) 
+        _G.a = bool 
         while wait(0) and _G.a do
-            if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
-            game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):Activate()
+         local tool = game.Players.LocalPlayer.Backpack:FindFirstChildOfClass("Tool") or game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
+    game.Players.LocalPlayer.Character.Humanoid:EquipTool(tool)
+    if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
+        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):Activate()
     end
-end
-
-    else
-        _G.a = false
     end
 end)
 
-MISC:CreateToggle("Speed Hack 100", function(arg) --the (arg) is if the checkbox is toggled or not
+MISC:CreateToggle("Speed Hack 100", function(arg) 
     if arg then
 _G.Speed = true 
         while wait() and _G.Speed do
@@ -82,32 +66,21 @@ _G.Speed = true
          end
 end)
 
-AutoTab:CreateToggle("Auto Buy Cake", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.c = true 
+AutoTab:CreateToggle("Auto Buy Cake", function(bool) 
+        _G.c = bool 
         while wait(0) and _G.c do
             local A_1 = "UpCake"
             local Event = game:GetService("ReplicatedStorage").Trigger
             Event:FireServer(A_1)
-
-        end
-    else
-        _G.c = false
     end
 end)
 
-AutoTab:CreateToggle("Auto Buy Sac", function(arg) --the (arg) is if the checkbox is toggled or not
-    if arg then
-        _G.d = true 
+AutoTab:CreateToggle("Auto Buy Sac", function(bool) 
+        _G.d = bool 
         while wait(0) and _G.d do
             local A_1 = "UpPlate"
             local Event = game:GetService("ReplicatedStorage").Trigger
             Event:FireServer(A_1)
-
-
-        end
-    else
-        _G.d = false
     end
 end)
 
