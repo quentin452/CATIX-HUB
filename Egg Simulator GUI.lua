0e729393d991a314f70683843db141e520a8fa18
quentin452 <42176772+quentin452@users.noreply.github.com>
2021-03-11 23:05:23 +0100

    Update Egg Simulator GUI.lua

--- Egg Simulator GUI.lua
+++ Egg Simulator GUI.lua
@@ -1,3 +1,4 @@
+local rs = game:GetService("RunService").RenderStepped
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
 
 local AutoTab = Library:CreateTab("Egg Simulator GUI","things", true)
@@ -5,176 +6,59 @@ local buy = Library:CreateTab("Auto Buy","things", true)
 local MISC = Library:CreateTab("MISC","things", true)
 local credits = Library:CreateTab("Credits","things", true)
 
-
-
-credits:CreateButton("Our Discords", function() --you dont need "arg" for a button
+credits:CreateButton("Our Discords", function() 
   setclipboard("AHAHAHAHAH")
 end)
-
-credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() --you dont need "arg" for a button
+credits:CreateButton("https://discord.gg/KmHZUpXEmQ", function() 
   setclipboard("https://discord.gg/KmHZUpXEmQ")
 end)
-
-credits:CreateButton("by Altix#3395", function() --you dont need "arg" for a button
+credits:CreateButton("by Altix#3395", function() 
   setclipboard("Altix#3395")
 end)
-
-credits:CreateButton("by I'm A Cat#7202", function() --you dont need "arg" for a button
+credits:CreateButton("by I'm A Cat#7202", function() 
   setclipboard("I'm A Cat#7202")
 end)
 
-buy:CreateToggle("Egg Multiplier FREE", function(arg) --the (arg) is if the checkbox is toggled or not
+buy:CreateToggle("Egg Multiplier FREE", function(arg) 
     if arg then
         _G.a = true 
-        while wait(0) and _G.a do
-            local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-            local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-            local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-            local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-
-
-            local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-            local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-          local args = {
-    [1] = 0,
-    [2] = "Egg Multiplier",
-}
-game:GetService("ReplicatedStorage").ShopBuy:FireServer(unpack(args))
-
-
-
- 
-        end
+        while wait(0.0001) and _G.a do
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+game:GetService("ReplicatedStorage").ShopBuy:FireServer(0,"Egg Multiplier")
+       end
     else
         _G.a = false
     end
 end)
 
-MISC:CreateToggle("NO CLIP Press(E)", function(arg) --the (arg) is if the checkbox is toggled or not
+MISC:CreateToggle("NO CLIP Press(E)", function(arg) 
     if arg then
 _G.NO = true 
   noclip = false
@@ -186,24 +70,17 @@ end)
 plr = game.Players.LocalPlayer
 mouse = plr:GetMouse()
 mouse.KeyDown:connect(function(key)
-
 if key == "e" then
 noclip = not noclip
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
 end
-end)
-print('Created by FileExplorer')
-print('Credits to Peyton for the ChangeState part of this script')
-
-         
+end) 
 else
 _G.NO = false
 end
 end)
 
-
-
-MISC:CreateToggle("Speed Hack 100", function(arg) --the (arg) is if the checkbox is toggled or not
+MISC:CreateToggle("Speed Hack 100", function(arg) 
     if arg then
 _G.Speed = true 
         while wait() and _G.Speed do
@@ -214,10 +91,10 @@ _G.Speed = true
            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 20
          end
 end)
-AutoTab:CreateToggle("Auto Rebirths", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Rebirths", function(arg) 
     if arg then
         _G.c = true 
-        while wait(0) and _G.c do
+        while wait(0.1) and _G.c do
           game:GetService("ReplicatedStorage").Rebirth:FireServer()
         end
     else
@@ -225,7 +102,7 @@ AutoTab:CreateToggle("Auto Rebirths", function(arg) --the (arg) is if the checkb
     end
 end)
 
-AutoTab:CreateToggle("Auto Collect White Eggs", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Collect White Eggs", function(arg) 
     if arg then
         _G.d = true 
         while wait(1) and _G.d do
@@ -246,10 +123,8 @@ local children = game.Workspace:GetChildren()
                if child:IsA("BasePart") and child.Name == "Egg2" then
             child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame 
           else
-               if child:IsA("BasePart") and child.Name == "Egg" then
-            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame 
-           
-
+            if child:IsA("BasePart") and child.Name == "Egg" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame     
     end
     end
     end
@@ -264,7 +139,7 @@ end)
 
 
 
-AutoTab:CreateToggle("Auto Collect Silver Eggs", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Collect Silver Eggs", function(arg) 
     if arg then
         _G.e = true 
         while wait(1) and _G.e do
@@ -287,8 +162,6 @@ local children = game.Workspace:GetChildren()
             else
              if child:IsA("BasePart") and child.Name == "SilverEgg" then
             child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame 
-           
-
     end
     end
     end
@@ -301,7 +174,7 @@ local children = game.Workspace:GetChildren()
     end
 end)
 
-AutoTab:CreateToggle("Auto Collect Gold Eggs", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Collect Gold Eggs", function(arg) 
     if arg then
         _G.f = true 
         while wait(1) and _G.f do
@@ -324,10 +197,10 @@ if child:IsA("BasePart") and child.Name == "GoldEgg" then
             else
                if child:IsA("BasePart") and child.Name == "GoldEgg5" then
             child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame     
-        end
  
-    end
-             end
+        end
+     end
+   end
 end
 end
 end
@@ -337,7 +210,7 @@ end
     end
 end)
 
-AutoTab:CreateToggle("Auto Collect Mythic Eggs", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Collect Mythic Eggs", function(arg) 
     if arg then
         _G.g = true 
         while wait(1) and _G.g do
@@ -365,7 +238,7 @@ end
     end
 end)
 
-AutoTab:CreateToggle("Auto Collect Supreme Eggs", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Collect Supreme Eggs", function(arg) 
     if arg then
         _G.h = true 
         while wait(1) and _G.h do
@@ -389,10 +262,10 @@ else
     end
 end)
 
-AutoTab:CreateToggle("Auto Collect Ultimate Eggs", function(arg) --the (arg) is if the checkbox is toggled or not
+AutoTab:CreateToggle("Auto Collect Ultimate Eggs", function(arg) 
     if arg then
         _G.i = true 
-        while wait(1) and _G.i do
+        while wait() and _G.i do
 local children = game.Workspace:GetChildren()
     for _, child in pairs(children) do
         for _, child in pairs(child:GetChildren()) do
@@ -412,18 +285,63 @@ end
     end
 end)
 
-teleports:CreateButton("Spawn", function() --you dont need "arg" for a button
+teleports:CreateButton("Spawn", function() 
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(71.7898483, 3.66993809, 83.2115402)
 end)
 
-teleports:CreateButton("Shop", function() --you dont need "arg" for a button
+teleports:CreateButton("Shop", function() 
 game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(216.487625, 3.76654005, 43.1189423)
 end)
+--| Anti Ban |
+setfflag("DFStringCrashPadUploadToBacktraceToBacktraceBaseUrl", "")
+setfflag("DFIntCrashUploadToBacktracePercentage", "0")
+setfflag("DFStringCrashUploadToBacktraceBlackholeToken", "")
+setfflag("DFStringCrashUploadToBacktraceWindowsPlayerToken", "")
 
---| Anti AFK | By Altix#3395
-    local Virtual = game:service'VirtualUser'
-    game:service'Players'.LocalPlayer.Idled:connect(function()
-        Virtual:CaptureController()
-        Virtual:ClickButton2(Vector2.new())
-        wait(2)
-    end)
+--| Anti Chat Logs |
+local CloneFunction = clonefunction
+local CheckCaller = CloneFunction(checkcaller)
+local HookFunction = CloneFunction(hookfunction)
+local LocalPlayer = game.Players.PlayerAdded:wait()
+
+local PostMessage = require(LocalPlayer:WaitForChild("PlayerScripts", 1/0):WaitForChild("ChatScript", 1/0):WaitForChild("ChatMain", 1/0)).MessagePosted
+getgenv().MessageEvent = Instance.new("BindableEvent")
+
+local OldFunctionHook
+local PostMessageHook = function(self, msg)
+   if not CheckCaller() and self == PostMessage then
+       MessageEvent:Fire(msg)
+       return
+   end
+   return OldFunctionHook(self, msg)
+end
+OldFunctionHook = HookFunction(PostMessage.fire, PostMessageHook)
+
+-- Anti AFK
+local Virtual = game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+Virtual:CaptureController()
+Virtual:ClickButton2(Vector2.new())
+wait(2)
+end)
+--| Anti KICK |
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+local protect = newcclosure or protect_function
+
+if not protect then
+protect = function(f) return f end
+end
+
+setreadonly(mt, false)
+mt.__namecall = protect(function(self, ...)
+local method = getnamecallmethod()
+if method == "Kick" then
+wait(9e9)
+return
+end
+return old(self, ...)
+end)
+hookfunction(game:GetService("Players").LocalPlayer.Kick,protect(function() wait(9e9)
+end))
+
